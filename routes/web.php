8821b6e3a5820b359e089c8622dc7786d8a037e0
Adriano <adrianosenny@gmail.com>
2022-12-07 11:51:11 -0300

    Criando rota do método update

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::post('/store', [PessoaController::class, 'store'])->name('pessoas.store')
 Route::get('/show/{id}', [PessoaController::class, 'show'])->name('pessoas.show');
 
 Route::get('/edit/{id}', [PessoaController::class, 'edit'])->name('pessoas.edit');
+
+Route::post('/update/{id}', [PessoaController::class, 'update'])->name('pessoas.update');
