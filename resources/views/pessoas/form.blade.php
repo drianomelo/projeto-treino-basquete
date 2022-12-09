23baafdc2b76589517a2dc1e7bf52b027cc58220
Adriano Melo <adrianosenny@gmail.com>
2022-12-09 07:49:58 -0300

    Criando e configurando views form e index

--- resources/views/pessoas/form.blade.php
+++ resources/views/pessoas/form.blade.php
@@ -1,8 +1,29 @@
 <!DOCTYPE html>
+<html lang="pt-br">
 
-@if(isset($pessoa)){
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @if (isset($pessoa))
+        <title>Treino - Visualização do treino</title>
+    @else
+        <title>Treino - Inscrição no treino</title>
+    @endif
+</head>
+
+<body>
+    @csrf
+    <label for="nome" class="form-check-label">
+        Nome:
+        <input type="text" name="nome" id="nome" value="{{ $pessoa->nome ?? null }}" placeholder="Somente letras">
+    </label>
+</body>
+
+</html>
+
+{{-- @if (isset($pessoa)){
     {{$pessoa}}
 }
 @endif
 
-{{$treinos}}
+{{$treinos}} --}}
--- resources/views/pessoas/index.blade.php
+++ resources/views/pessoas/index.blade.php
@@ -1,3 +1,43 @@
 <!DOCTYPE html>
-{{$pessoas}}
+<html lang="pt-br">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Treinos</title>
+</head>
+
+<body>
+    <main class="main">
+        <table class="table">
+            <thead>
+                <h1>Lista de pessoas</h1>
+                <a href="{{ route('pessoas.create') }}">Entrar</a>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Posição</th>
+                    <th scope="col">Altura</th>
+                    <th scope="col">Peso</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($pessoas as $pessoa)
+                    <tr>
+                        <td>{{ $pessoa->nome }}</td>
+                        <td>{{ $pessoa->posicao }}</td>
+                        <td>{{ $pessoa->atributo->altura }}</td>
+                        <td>{{ $pessoa->atributo->peso }}</td>
+                        <td>
+                            <a href="{{ route('pessoas.edit', $pessoa->id) }}">Editar</a>
+                            <a href="{{ route('pessoas.show', $pessoa->id) }}">Visualizar</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </main>
+</body>
+
+</html>
+{{-- {{ $pessoas }} --}}
