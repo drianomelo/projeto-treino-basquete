aceb836d81b8313c8238ee48cf3ec8d4af0747d0
Adriano <adrianosenny@gmail.com>
2022-12-14 11:46:11 -0300

    Adicionando estilos ao index

--- resources/views/pessoas/index.blade.php
+++ resources/views/pessoas/index.blade.php
@@ -6,52 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Treinos disponíveis</title>
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
     <main class="main">
-        <div class="main__flex">
-            <h2 class="main__title">Treinos disponíveis</h2>
-            <div class="main__cards">
-                @foreach ($treinos as $treino)
-                    <div class="main__card">
-                        <img src="img/prancheta.png" alt="">
-                        
-                            <a href="{{ route('pessoas.show', $pessoas->id) }}">Inscritos</a>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+        <div class="main__card">
+            <img src="img/prancheta.png" alt="">
             <a href="{{ route('pessoas.create') }}" class="main__btn">Inscrever-se</a>
+            <div class="main__infos">
+                <table>
+                    <thead>
+                        <tr>
+                            <th class="coluna" scope="col">Nome</th>
+                            <th class="coluna" scope="col">Posição</th>
+                            <th class="coluna" scope="col">Altura</th>
+                            <th class="coluna" scope="col">Peso</th>
+                            <th class="coluna" scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($pessoas as $pessoa)
+                            <tr>
+                                <td>{{ $pessoa->nome }}</td>
+                                <td>{{ $pessoa->posicao }}</td>
+                                <td>{{ $pessoa->atributo->altura }}</td>
+                                <td>{{ $pessoa->atributo->peso }}</td>
+
+                                <td>
+                                    <a href="{{ route('pessoas.edit', $pessoa->id) }}">Editar</a>
+                                    <a href="{{ route('pessoas.show', $pessoa->id) }}">Visualizar</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
-    {{-- <table class="table">
-            <thead>
-                <h1>Lista de pessoas</h1>
-                <a href="{{ route('pessoas.create') }}">Entrar</a>
-                <tr>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Posição</th>
-                    <th scope="col">Altura</th>
-                    <th scope="col">Peso</th>
-                    <th scope="col">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($pessoas as $pessoa)
-                    <tr>
-                        <td>{{ $pessoa->nome }}</td>
-                        <td>{{ $pessoa->posicao }}</td>
-                        <td>{{ $pessoa->atributo->altura }}</td>
-                        <td>{{ $pessoa->atributo->peso }}</td>
-                        <td>
-                            <a href="{{ route('pessoas.edit', $pessoa->id) }}">Editar</a>
-                            <a href="{{ route('pessoas.show', $pessoa->id) }}">Visualizar</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
